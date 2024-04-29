e75a177d84d1d4dc6d82aa1197c249fdfc6551ed
FelekDevYT <157293851+FelekDevYT@users.noreply.github.com>
2024-04-29 13:07:54 +0300

    FelsOS 0.0.2

--- FelsOS/Commander.cs
+++ FelsOS/Commander.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization.Metadata;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FelsOS
@@ -13,6 +14,19 @@ namespace FelsOS
         {
             Console.WriteLine(message);
         }
+
+        public static void RDS()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+            Console.WriteLine("You computer created some problems or\ncomputer don't have permission!");
+            Console.WriteLine("Reload computer for debug problem!");
+            Console.Write("Press any key to reboot system...");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+            Cosmos.System.Power.Reboot();
+        }
+
         public static void help()
         {
             Console.WriteLine(@"Commands of FelsOS :
@@ -29,7 +43,12 @@ read - read text from file
 sysinfo - system information
 now - return now time
 shutdown - shutdown you computer
-reload - reboot you computer");
+reload - reboot you computer
+st - show tascs
+kill - kill process(ID)
+net - network commands
+rp - refactor password
+buffer - commands for refactoring,saving buffer");
         }
     }
 }
--- FelsOS/Kernel.cs
+++ FelsOS/Kernel.cs
@@ -7,6 +7,12 @@ using System.IO;
 using Cosmos.System.FileSystem;
 using System.Threading;
 using System.Security;
+using Cosmos.System.Graphics;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Numerics;
+using System.Threading.Tasks;
+using Cosmos.System.Network.Config;
 
 namespace FelsOS
 {
@@ -14,16 +20,68 @@ namespace FelsOS
     {
         Sys.FileSystem.CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
         String current_directory = "0:\\";
+        int ty = 0;
+        String[] tascs = new string[128];
+        String[] tascs_names = new String[128];
+        int ct = 0;
+        String buffer;
+        const String version = "0.0.2";
+
         protected override void BeforeRun()
         {
-            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
             Console.Clear();
             Console.WriteLine("Starting booting...");
             Thread.Sleep(1000);
             Console.WriteLine("Starting FelsOS...");
-            Thread.Sleep(450);
-            Console.WriteLine("FelsOS is start>>>VERSION 0.0.1>>>CREATOR ::: FELEK_");
-            Thread.Sleep(450);
+            Thread.Sleep(1000);
+            Console.WriteLine("FelsOS is start>>>VERSION" + version + ">>> CREATOR ::: FELEK_");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+            tascs[ct] = "000001";
+            tascs_names[ct] = "System";
+            ct++;
+            tascs[ct] = "000236";
+            tascs_names[ct] = "FileSystem";
+            ct++;
+            tascs[ct] = "000008";
+            tascs_names[ct] = "REGSX";
+
+            if (Directory.Exists(@"0:\User"))
+            {
+                String[] user = File.ReadAllLines(@"0:\User\user.fsf");
+                int passwordUse = 0;
+                String textLogin;
+                while(passwordUse != 3)
+                {
+                    Console.Write("Enter password>");
+                    textLogin = Console.ReadLine();
+                    if(textLogin == user[1])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed login!");
+                    }
+                    passwordUse++;
+                }
+                if(passwordUse == 3)
+                {
+                    Cosmos.System.Power.Reboot();
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(@"0:\User");
+                String userName = "DEFAULT_USER";
+                String userPassword = "password!";
+                Console.Write("Enter you name>");
+                userName = Console.ReadLine();
+                Console.Write("Enter you password>");
+                userPassword = Console.ReadLine();
+                File.WriteAllText(@"0:\User\user.fsf", userName + "\n" + userPassword);
+            }
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.DarkMagenta;
             Console.WriteLine("FelsOS " + DateTime.Now);
@@ -35,10 +93,22 @@ namespace FelsOS
 
         protected override void Run()
         {
+            if (tascs[0] == null)
+            {
+                Commander.RDS();
+            }else if (tascs[1] == null)
+            {
+                Commander.RDS();
+            }else if (tascs[2] == null)
+            {
+                Commander.RDS();
+            }
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.Write("#"+current_directory+"#FelsOS$");
             Console.ForegroundColor = ConsoleColor.White;
-            commands = Console.ReadLine().Split(' ');
+            String s;
+            s = Console.ReadLine();
+            commands = s.Split(' ');
             switch (commands[0])
             {
                 case "echo":
@@ -137,9 +207,97 @@ Used RAM : {4}", br, pp, gg, un, nt);
                 case "reload":
                     Cosmos.System.Power.Reboot();
                     break;
-                case "felsInt":
-                    Console.WriteLine("Welcome to The fels - programming language for FelsOS!");
-                    interpreter.interpreate();
+                case "st":
+                    foreach (String el in tascs)
+                    {
+                        if(el == null)
+                        {
+                            continue;
+                        }
+                        Console.WriteLine("Element : "+el);
+                    }
+                    break;
+                case "kill":
+                    int use = 0;
+                    foreach(String el in tascs)
+                    {
+                        if (commands[1] == el)
+                        {
+                            Console.WriteLine("Process " + commands[1] + " - successful killed");
+                            tascs[use] = null;
+                            tascs_names[use] = null;
+                        }
+                        use++;
+                    }
+                    break;
+                case "net":
+                    if (commands[1] == "IP")
+                    {
+                        Console.WriteLine("Current IP: "+NetworkConfiguration.CurrentAddress.ToString());
+                    }
+                    break;
+                case "rp"://rp youPassword newPassword
+                    String[] user = File.ReadAllLines(@"0:\User\user.fsf");
+                    if (user[1] == commands[1])
+                    {
+                        File.WriteAllText(@"0:\User\user.fsf",user[0] + "\n" + commands[2]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed enter password!");
+                    }
+                    break;
+                case "buffer":
+                    switch (commands[1])
+                    {
+                        case "set":
+                            String[] test = s.Split('\"');
+                            buffer = test[1];
+                            break;
+                        case "save":
+                            File.WriteAllText(@"0:\User\buffer.fuf",buffer);
+                            break;
+                        case "open":
+                            buffer = File.ReadAllText(@"0:\User\buffer.fuf");
+                            break;
+                        case "read":
+                            Console.WriteLine(buffer);
+                            break;
+                    }
+                    break;
+                case "REGSX":
+                    Commander.RDS();
+                    break;
+                case "notepad":
+                    String[] lns = new string[8];
+                    use = 0;
+                    while (true)
+                    {
+                        lns[use] = Console.ReadLine();
+                        if (lns[use] == "exit")
+                        {
+                            break;
+                        }
+                        use++;
+                    }
+                    try
+                    {
+                        use = 0;
+                        while (use < 1024)
+                        {
+                            if (lns[use] != null)
+                            {
+                                File.AppendAllText(@"" + commands[1], lns[use]);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }catch(Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
                     break;
             }
         }
